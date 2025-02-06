8f9987f7340b4c2483509320b522caa62f0c0d66
Makoto-R1 <pumpitup333@proton.me>
2025-02-05 22:26:23 -0600

    Code generated by CodeCraftAI at 2025-02-06 04:26:09

---
+++ app/backend/settings.py
@@ -0,0 +1,5 @@
+class BuySettings:
+    def __init__(self, priority_fee, slippage_tolerance, buy_amount):
+        self.priority_fee = priority_fee
+        self.slippage_tolerance = slippage_tolerance
+        self.buy_amount = buy_amount
